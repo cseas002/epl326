80c077c6e87e64d19b1f1b1c952fa59f98a389ea
cseas002 <cseas002@ucy.ac.cy>
2023-02-21 15:26:22 +0200

    done homework 1

--- Homeworks/Homework_1/puzzles/bash.sh
+++ Homeworks/Homework_1/puzzles/bash.sh
@@ -2,10 +2,10 @@
 
 for key in {0..1000000}
 do
-openssl aes-128-cbc -d -in puzzle5 -out output.txt -k $key 2> /dev/null
+openssl aes-128-cbc -d -in puzzle5 -out output.txt -k "$key" 2> /dev/null
 check=$(file output.txt)
 if [[ $check == *"ASCII"* ]]; then
-  echo $key;
-  openssl aes-128-cbc -d -in puzzle5 -out $[key]output.txt -k $key;
+  echo "$key";
+  openssl aes-128-cbc -d -in puzzle5 -out $((key))output.txt -k "$key";
 fi
 done
