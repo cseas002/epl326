0fde2fdd498bdf383225ba53ee6aec7a5407050b
cseas002 <cseas002@ucy.ac.cy>
2023-02-24 11:55:12 +0200

    done homework 1 documented

--- Homeworks/Homework_1/decryption_methods.py
+++ Homeworks/Homework_1/decryption_methods.py
@@ -1,8 +1,8 @@
 def find_char(key, characters, character):
     """
-    :param key: the key value (amount of letters to shift)
+    :param key: the key value (amount of characters to shift)
     :param characters: the characters list (list with ' ' and from 'a' to 'z'
-    :param character: the character to be shifted with $key letters to the right (clockwise)
+    :param character: the character to be shifted with $key characters to the right (clockwise)
     :return: the character in this cipher
     """
     index = characters.index(character)  # find the index of the character
@@ -36,6 +36,12 @@ def transposition(msg, rows, cols):
 
 
 def one_key_substitution(msg, characters):
+    """
+    Substitute each character based on a key (shift) in `characters` alphabet
+    :param msg: the message to decode
+    :param characters: the characters list (alphabet)
+    :return: list of all the answers (for each possible key)
+    """
     answers = []
     for key in range(len(characters)):  # for every possible key (which is the number of characters to pass
         # (e.g. for key = 1, message = 'a', the decoded message will be 'b'
@@ -45,13 +51,20 @@ def one_key_substitution(msg, characters):
                 decoded_message += '\n'
             else:
                 decoded_message += find_char(key, characters, char)
-            # find the right letter to be substituted and add it to the decoded string
+            # find the right character to be substituted and add it to the decoded string
         answers.append(decoded_message)
 
     return answers
 
 
 def two_key_substitution(msg, characters):
+    """
+    Substitute each character based on two keys (shift) in `characters` alphabet
+    :param msg: the message to decode
+    :param characters: the characters list (alphabet)
+    :return: 2D list of all the answers (for each possible key combinations)
+    e.g. answers[1][2] = key1 = 1 and key2 = 2
+    """
     answers = [['0'] * len(characters) for _ in range(len(characters))]  # initializing the answers matrix
     for key in range(len(characters)):
         for key2 in range(len(characters)):
@@ -71,6 +84,13 @@ def two_key_substitution(msg, characters):
 
 
 def three_key_substitution(msg, characters):
+    """
+    Substitute each character based on three keys (shift) in `characters` alphabet
+    :param msg: the message to decode
+    :param characters: the characters list (alphabet)
+    :return: 3D list of all the answers (for each possible key combinations)
+    e.g. answers[1][2][3] = key1 = 1, key2 = 2 and key3 = 3
+    """
     file = open('answers3.txt', 'w')
 
     # answers = [[['0'] * len(characters) for _ in range(len(characters))] for _ in range(len(characters))]
@@ -83,15 +103,15 @@ def three_key_substitution(msg, characters):
                 for i in range(0, len(msg), 3):  # for every character in the file
                     if msg[i] == '\n':  # ignore new line character
                         decoded_message += '\n'
-                    elif msg[i + 1] == '\n':  # ignore new line character
-                        decoded_message += find_char(key, characters, msg[i])
+                    elif msg[i + 1] == '\n':  # if the second character is the new line character
+                        decoded_message += find_char(key, characters, msg[i])  # substitute the first character
                         decoded_message += '\n'
-                    elif msg[i + 2] == '\n':  # ignore new line character
-                        decoded_message += find_char(key, characters, msg[i])
+                    elif msg[i + 2] == '\n':  # if the third character is the new line character
+                        decoded_message += find_char(key, characters, msg[i])  # substitute the two first characters
                         decoded_message += find_char(key2, characters, msg[i + 1])
                         decoded_message += '\n'
                     else:
-                        decoded_message += find_char(key, characters, msg[i])
+                        decoded_message += find_char(key, characters, msg[i])  # substitute the three characters
                         decoded_message += find_char(key2, characters, msg[i + 1])
                         decoded_message += find_char(key3, characters, msg[i + 2])
                 # answers[key][key2][key3] = decoded_message
